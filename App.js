a0ac82c896576dc7a8590df19fc27b3c1e209d5b
Hasan Huda <hasanhuda037@gmail.com>
2023-05-09 14:37:24 -0400

    implemented text inputs

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Yea</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your Course Goal!"/>
+        <Button title="Add Goal"/>
+      </View>
+      <View>
+        <Text>List of Goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer:{
+    padding:50
   },
+  inputContainer:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput:{
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 4
+  }
 });
